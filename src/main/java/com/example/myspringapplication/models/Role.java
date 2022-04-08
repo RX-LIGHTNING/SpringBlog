25e4559ab06d59038bc39485a7219ab8ce4c19e0
Roman Kuznetsov <roman_kuzn@mail.ru>
2022-04-08 23:38:33 +0300

    Separated admin role to authorities

--- src/main/java/com/example/myspringapplication/models/Role.java
+++ src/main/java/com/example/myspringapplication/models/Role.java
@@ -1,5 +1,18 @@
 package com.example.myspringapplication.models;
 
 public enum Role {
-    USER,ADMIN,CANDELETECOMMENTS;
+    USER("Casual role"),
+    ADMIN("Role, that gives access to users list"),
+    CANDELETECOMMENTS("Role, that gives permission to delete comment"),
+    CANEDITTOPIC("Role, that gives permission to edit topics"),
+    CANDELETETOPIC("Role, that gives permission to delete topics");
+    String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    Role(String description) {
+        this.description = description;
+    }
 }
